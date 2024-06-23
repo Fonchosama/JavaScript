3cdb24d1ea252ec23e9ae12b34e533e55fcea01a
GianBo <162351219+Fonchosama@users.noreply.github.com>
2024-06-23 22:44:06 +0200

    Update scripts.js
    if applied, it will add the information of 3 pokemons information in the script file ( name / height/ type

---
+++ scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+  { name: 'bulbasaur', height: '70cm', type: ['grass', 'poison'] },
+  { name: 'charmander', height: '60cm', type: ['fire'] },
+  { name: 'squirtle', height: '50cm', type: ['water'] },
+];
