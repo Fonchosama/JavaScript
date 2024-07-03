285cdc1916123a557ebc0de57e44dac65d4757b9
GianBo <162351219+Fonchosama@users.noreply.github.com>
2024-07-03 23:18:41 +0200

    Update scripts.js
    if applied, the script will now write on the DOM a text that will say "wow that is big"

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
 let pokemonList = [
-  { name: 'bulbasaur', height: '70cm', type: ['grass', 'poison'] },
-  { name: 'charmander', height: '60cm', type: ['fire'] },
-  { name: 'squirtle', height: '50cm', type: ['water'] },
+  { name: 'bulbasaur', height: 0.7, type: ['grass', 'poison'] },
+  { name: 'charmander', height: 0.6, type: ['fire'] },
+  { name: 'squirtle', height: 0.5, type: ['water'] },
 ];
 
-for (i = 0; i < pokemonList.length; i++) {
+for (let i = 0; i < pokemonList.length; i++) {
   document.write(
-    pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') /'
+    pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') '
   );
+  if (pokemonList[i].height > 0.6) {
+    document.write(' - wow, that is big!');
+  }
 }
